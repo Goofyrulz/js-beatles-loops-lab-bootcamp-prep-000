904984b11a5019bd8f54b3050df5cc3ed2660320
Christopher Anderson <andersonchris227@gmail.com>
2018-09-07 18:39:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,32 @@ function johnLennonFacts(facts) {
 }
 return excitedFacts;
 }
+
+
+
+ 	function iLoveTheBeatles(n) {
+
+		const arr = [];
+
+ 	do {
+		arr.push('I love the Beatles!');
+		n++;
+
+	} 
+	while (n > 15 );
+	
+ 	return arr;
+}
+  
+   
+  // describe('iLoveTheBeatles', function() {
+  //   it("returns an array of 'I love the Beatles!' 8 times when passed the parameter 7 ", function() {
+  //     expect(iLoveTheBeatles(7)).toEqual(["I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!"]);
+  //   });
+
+  //   it("returns an array of 'I love the Beatles!' once when passed the parameter 17", function() {
+  //     expect(iLoveTheBeatles(17)).toEqual(["I love the Beatles!"]);
+  //   });
+  // });
+
+// });
